0e3e9f95c97afca9ad8c25b6cf24cf18ecb0b4bb
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-02-17 14:01:21 +0300

    gtest

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/WORK/WORK-LOG
+# Build directory: /mnt/d/WORK/WORK-LOG/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/mnt/d/WORK/WORK-LOG/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
