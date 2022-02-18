43e9446796bcdb9f17816d16d3d848c213d96a13
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-02-18 18:51:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <gtest/gtest.h>
 #include <iostream>
- using namespace std;
+using namespace std;
 
- int main(void) {
+int main(void) {
    cout << "Hello, World\n";
    cout << "Hello, Jenkins\n";
-
    cout << "I have successfully built and run\n";
    return 0;
 }
 
+// Bir jenkins hattımız olsun
+// Repodan git clone yapsın
+// cpp kodlarını derlesin. make build
+  // dinamik olması gereken isimler parametrik olsun
+  // Agent ile çalışsın
+// cpp kodlarını test etsin. make test
+// Distribute etsin. deb pakti oluşucak.
 
+
+// Kubernetes HELM CHART var
+// Prometheus, Grafana, Loki, Elasticsearch 
