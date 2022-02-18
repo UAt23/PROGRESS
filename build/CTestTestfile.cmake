f55d21141a65273db6ccca44c324275112b20806
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-02-18 13:36:31 +0300

    rebuilding

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/WORK/WORK-LOG
-# Build directory: /mnt/d/WORK/WORK-LOG/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/d/WORK/WORK-LOG/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( HelloTest.BasicAssertions /mnt/d/WORK/WORK-LOG/build/hello_test [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties( HelloTest.BasicAssertions PROPERTIES WORKING_DIRECTORY /mnt/d/WORK/WORK-LOG/build)
-set( hello_test_TESTS HelloTest.BasicAssertions)
