c5319e88c3f0d60b7c8ca852e39c02e0f46bde97
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-02-21 13:21:23 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "triangle.h"
 
-
+int TypeOfTriangle(int a, int b, int c) {
+            if (a < 0 || b < 0 || c < 0) return -1;
+            if (!(a + b > c && b + c > a && a + c > b))
+                return 0;
+            else if (a == b && b == c)
+                return 1;
+            else if (a == b || b == c || c == a)
+                return 2;
+            else
+                return 3;            
+        }
 
 int main(){
 	Triangle shw;
 	shw.sayHelloWorld();
-	shw.TypeOfTriangle(10, 25, 18);
+	TypeOfTriangle(10, 25, 18);
 }
