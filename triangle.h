408414497d8f5df6eba56edb0b7ccf04e88c63fc
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-02-21 12:41:00 +0300

    Update triangle.h

--- triangle.h
+++ triangle.h
@@ -1,6 +1,15 @@
 #ifndef __TRIANGLE_H_
 #define __TRIANGLE_H_
 
+#include <iostream>
+#include <stdlib.h>
+
+
+void sayHelloWorld(){
+			std::cout << "I Say Hello World!" << std::endl;
+		}
+
+
 int TypeOfTriangle(int, int, int);
 
 #endif
